5652bd105305a75d559a11e43caef19e85fa2427
Michael T. Wolfinger <michael@wolfinger.eu>
2022-02-01 23:58:02 +0100

    refactor for new gap-only/high-gap frequency filtering script

--- bin/pp_RNALalifold.sh
+++ bin/pp_RNALalifold.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Last changed Time-stamp: <2022-01-19 23:53:39 mtw>
+# Last changed Time-stamp: <2022-02-01 23:26:10 mtw>
 
 # This can be used as a RNALalifold post-processor:
 # this script post-processes a set of Stockholm files by doing
@@ -28,9 +28,10 @@ function check_infernal {
 }
 
 wd=$(pwd)
-CM=OFF
-STRIP=OFF
-LOCARNATE=OFF
+CM=OFF         # flag for building a CM
+STRIP=OFF      # flag for stripping redundant lines from MSA
+REMGAP=OFF     # flag for removing gap-only/high-gap fraction lines from MSA
+LOCARNATE=OFF  # flag for making RNAz LocARNA-aware
 log="${wd}/pp_RNALalifold.log"
 logcsv="${wd}/pp_RNALalifold.log.csv"
 touch $log
@@ -38,13 +39,14 @@ touch $logcsv
 
 for i in "$@"
 do
-  case $i in
-    -l)          # make RNAz locarna-aware
-	LOCARNATE=ON
+    case $i in
+    -g)             # remove gap-only/high-gap lines from MSA
+	REMGAP=ON
 	shift
 	;;
-    -p=*)
-	P="${i#*=}" # RNAz class probability
+	
+    -l)             # make RNAz LocARNA-aware
+	LOCARNATE=ON
 	shift
 	;;
     -s)
@@ -66,24 +68,33 @@ for w in $(ls *stk)
 do
   wbn=$(basename $w .stk)
   echo "[I pp_RNALalifold] processing $wbn"
-  # $removegaponly -a $w > $wbn.clean.aln 2> /dev/null
+
+  cp ${wbn}.stk ${wbn}.orig.stk
+
+  if [[ "$REMGAP" == "ON" ]]; then
+      ${RC_removegaponly} -a ${wbn}.stk -i stockholm -r 0.5 > ${wbn}.remgap.stk 2> /dev/null
+      cp -f ${wbn}.remgap.stk ${wbn}.stk
+  fi
   #  $reformataln -a $w.clean.aln -i clustal -o clustal > $w.reformat.aln
 
   if [[ "$STRIP" == "ON" ]]; then
-    ${RC_stripaln} -a ${wbn}.stk -f S > ${wbn}.strip.stk
-    cp -f ${wbn}.strip.stk ${wbn}.stk
+      ${RC_stripaln} -a ${wbn}.stk -f S > ${wbn}.strip.stk
+      cp -f ${wbn}.strip.stk ${wbn}.stk
   fi
 
   ${RC_eslreformat} clustal ${wbn}.stk > ${wbn}.aln
+
+  # optional: realign stripped MSA with mlocarna here 
+
   RNAZ_OPTIONS="-d"
   if [[ "$LOCARNATE" == "ON" ]]; then
-    RNAZ_OPTIONS="${RNAZ_OPTIONS} -l"
+      RNAZ_OPTIONS="${RNAZ_OPTIONS} -l"
   fi
   ${RC_rnaz} ${RNAZ_OPTIONS} ${wbn}.aln > ${wbn}.rnaz.txt
   if [[ -s "${wbn}.rnaz.txt" ]]; then # grep SVM class probability
-    rnazprob=$(grep probability ${wbn}.rnaz.txt | perl -ne 's/[a-zA-Z\-\:\s]+//g;print')
+      rnazprob=$(grep probability ${wbn}.rnaz.txt | perl -ne 's/[a-zA-Z\-\:\s]+//g;print')
   else
-    rnazprob="-1"
+      rnazprob="-1"
   fi
 
   ${RC_alifoldz} -f -t 0.0 < ${wbn}.aln > ${wbn}.alifoldz.txt
